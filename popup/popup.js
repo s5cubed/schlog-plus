f477ef602de3485c718e5b247df90c7160c32814
sss5sss555s5s5s5 <s5s5s5wum@gmail.com>
2024-09-28 21:26:44 +0100

    I feel like someone is messing with me, files are out of date

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,27 @@
+// ===========================================================================
+// Schlog+ / popup.js
+// Created by s. aka Soygoy
+//
+// https://soyjak.blog/index.php?members/soygoy.488/
+// sss5sss555s5s5s5.github.com
+// s5s5s5.com
+//
+// This code is lcensed under the MIT License, please credit me in your forks.
+// ===========================================================================
+var browserType = "firefox"
+if (typeof browser === "undefined") {
+	var browser = chrome;
+	browserType = "chrome";
+}
+
 function onOpened() {
-    console.log(`Options page opened`);
+	console.log(`Options page opened`);
 }
 
 function onError(error) {
-    console.log(`Error: ${error}`);
+	console.log(`Error: ${error}`);
 }
 
-let opening = chrome.runtime.openOptionsPage();
+let opening = browser.runtime.openOptionsPage();
 opening.then(onOpened, onError);
 window.close();
